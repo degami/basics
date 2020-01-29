7eab97dd8c17098121eb93f003315eb0fa0cf6d7
Mirko De Grandis <degami@gmail.com>
2020-01-29 23:16:57 +0100

    added TagList that was missing

---
+++ src/Html/TagList.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Degami Basics
+ * PHP Version 7.0
+ *
+ * @category CMS / Framework
+ * @package  Degami\Basics
+ * @author   Mirko De Grandis <degami@github.com>
+ * @license  MIT https://opensource.org/licenses/mit-license.php
+ * @link     https://github.com/degami/sitebase
+ */
+namespace Degami\Basics\Html;
+
+use \Degami\Basics\Traits\ToolsTrait;
+use \Degami\Basics\Exceptions\BasicException;
+
+/**
+ * A class to render form fields tags
+ */
+class TagList extends BaseElement implements TagInterface
+{
+    use ToolsTrait;
+
+    /** @var array tag children */
+    protected $children;
+
+    /**
+     * Class constructor
+     *
+     * @param array $options build options
+     */
+    public function __construct($options = [])
+    {
+        $this->children = [];
+        $this->setClassProperties($options);
+    }
+
+    /**
+     * Gets html tag string
+     *
+     * @return string tag html representation
+     */
+    public function renderTag()
+    {
+        $out = "";
+        foreach ($this->children as $key => $value) {
+            if ($value instanceof TagInterface) {
+                $out .= $value->renderTag();
+            }
+        }
+        return $out;
+    }
+
+    /**
+     * Add child to tag
+     *
+     * @param  TagInterface $child child to add
+     * @return TagList
+     */
+    public function addChild($child)
+    {
+        if ($child instanceof TagInterface) {
+            $this->children[] = $child;
+        }
+        return $this;
+    }
+
+    /**
+     * toString magic method
+     *
+     * @return string the tag html
+     */
+    public function __toString()
+    {
+        try {
+            return $this->renderTag();
+        } catch (BasicException $e) {
+            return $e->getMessage()."\n".$e->getTraceAsString();
+        }
+    }
+}
