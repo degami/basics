580ee262a3afddedc24e781aa3b978dd8d66299f
Mirko De Grandis <degami@gmail.com>
2025-08-18 20:46:35 +0200

    commit

--- src/Traits/ToolsTrait.php
+++ src/Traits/ToolsTrait.php
@@ -61,12 +61,17 @@ trait ToolsTrait
      * @param  string
      * @return string
      */
-    public static function pascalCaseToSnakeCase(string $input) : string
+    public static function pascalCaseToSnakeCase(string $input): string
     {
-        preg_match_all('!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!', $input, $matches);
+        preg_match_all(
+            '/([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+|\d+[a-zA-Z]*\d*)/',
+            $input,
+            $matches
+        );
+
         $ret = $matches[0];
         foreach ($ret as &$match) {
-            $match = $match == strtoupper($match) ? strtolower($match) : lcfirst($match);
+            $match = $match === strtoupper($match) ? strtolower($match) : lcfirst($match);
         }
         return implode('_', $ret);
     }
